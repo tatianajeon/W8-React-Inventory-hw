70d88f68e865e510fb006ca1914cf3d1ae3e07c6
tatiana <105023527+tatianajeon@users.noreply.github.com>
2022-07-11 20:39:23 -0500

    first commit

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { styled } from '@mui/system';
+import { Button } from '@mui/material';
+import golfball from '../assets/images/golfball.jpg';
+import { Link } from 'react-router-dom';
+
+interface Props{
+    title:string;
+}
+
+const Root = styled("div")({
+    padding: 0,
+    margin: 0
+})
+const NavbarContainer = styled('div')( {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+})
+const Logo = styled('h1')({
+    margin: '0 0 0 0.45em'
+})
+const LogoA = styled(Link)( {
+    color: 'rgb(28,24,22)',
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none'
+})
+const LogoNavigation = styled('ul')( {
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    display: 'flex'
+})
+
+const NavA = styled(Link)({
+    display: 'block',
+    padding: '1em',
+    color: 'black'
+})
+const Main = styled('main')( {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${golfball});`,
+    width: '100%',
+    height: '100%',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'absolute',
+})
+const MainText = styled('div')({
+    textAlign: 'center',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white'
+})
+
+export const Home = ( props:Props) => {
+ 
+    return (
+        <Root>
+            <NavbarContainer>
+                <Logo>
+                    <LogoA to="/">The Perfect Swing</LogoA>
+                </Logo>
+                <LogoNavigation>
+                    <li>
+                        <NavA to="/">Home</NavA>
+                    </li>
+                    <li>
+                        <NavA to="/dashboard">Dashboard</NavA>
+                    </li>
+                    <li>
+                        <NavA to="signin">Sign In</NavA>
+                    </li>
+                </LogoNavigation>
+            </NavbarContainer>
+            <Main>
+                <MainText>
+                    <h1>{props.title}</h1>
+                    <p>Virtual Golfclub Bag</p>
+                    <Button color='primary' variant='contained'>golf clubs here</Button>
+                </MainText>
+            </Main>
+
+        </Root>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Dashboard, SignIn } from './components';
+import './styles.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home title  ={'The Perfect Swing'}/>} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/signin' element={<SignIn />} />
+        </Routes>
+      </Router>
+
+    
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
